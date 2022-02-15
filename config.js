c98bb8615166dc9e19ed63a3ec306b711a658632
Quentin Vansteene <vansteene.quentin@gmail.com>
2022-02-15 19:19:19 +0100

    add website color config

--- config.js
+++ config.js
@@ -1,1 +1,27 @@
-module.exports = {};
+module.exports = {
+	'colors' : {
+		'blue' : '#2878e4',
+		'yellow' : '#F2CA52',
+		'yellowlight' : '#f2d06b',
+		'yellowlighter' : '#ffff71',
+	},
+	'primary': 'colors(yellow)',
+	'secondary': 'colors(blacklight)',
+
+	'body': {
+		'background': 'colors(white)',
+		'block-background': 'colors(white)',
+		'font-color': 'colors(blacklight)',
+		// 'font-color-title': 'change-color(primary,$lightness:10%)',
+		// 'font-color-title': 'colors(greystronger)',
+	},
+	'header': {
+		'background': 'secondary',
+		'font-color': 'contrastFW(colors(white),secondary)',
+	},
+	'footer': {
+		'background': 'header(background)',
+		'font-color': 'header(font-color)',
+	},
+
+};
